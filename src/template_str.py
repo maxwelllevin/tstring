02b9c141074aee679d648fa2bf9484c59a3e9319
Maxwell Levin <24307537+maxwelllevin@users.noreply.github.com>
2022-12-23 10:20:18 -0800

    Initial commit

---
+++ src/template_str.py
@@ -0,0 +1,161 @@
+import re
+from typing import Dict, List, Match
+
+TEMPLATE = "{a}.{b}.{c}[-{d}]"
+
+mapping = {
+    "a": "hello",
+    "b": "there",
+    "c": "world",
+    "d": "Hah",
+}
+
+_SQUARE_BRACKET_REGEX = r"\[(.*?)\]"
+_CURLY_BRACKET_REGEX = r"\{(.*?)\}"
+
+
+def _substitute(
+    template: str,
+    mapping: Dict[str, str],
+    allow_missing: bool = False,
+    **kwds: str,
+) -> str:
+    """Substitutes variables in a template string.
+
+    The template string is expected to be formatted in the same way as python f-strings,
+    with variables that should be substituted wrapped in curly brances `{}`.
+    Additionally, square brackets may be used around curly brackets and other text to
+    mark that substitution as optional -- i.e. if the variable cannot be found then the
+    text wrapped in the square brackets will be removed.
+
+    Examples:
+
+        `mapping = dict(a="x", b="y", c="z")`
+
+        `substitute("{a}.{b}{c}w", mapping) == "x.yzw"  # True`
+
+        `substitute("{a}.{b}[.{c}]", mapping) == "x.y.z"  # True`
+
+        `substitute("{a}.{b}[.{d}]", mapping) == "x.y"  # True`
+
+        `substitute("{a}.{b}.{d}", mapping, True) == "x.y.{d}"  # True`
+
+        `substitute("{a}.{b}.{d}", mapping, False)  # raises ValueError
+
+    Args:
+        template (str): The template string. Variables to substitute should be wrapped
+            by curly braces `{}`.
+        mapping (Dict[str, str]): A key-value store of variable name to the value to
+            replace it with.
+        allow_missing (bool, optional): Allow variables outside of square brackets to be
+            missing, in which case they are left as-is, including the curly brackets.
+            This is intended to allow users to perform some variable substitutions
+            before all variables in the mapping are known. Defaults to False.
+        **kwds (Dict[str, str]): Optional extras to be merged into the variable mapping
+            dict. If a keyword passed here has the same name as a key in the mapping
+            dict, the value here would be used instead.
+
+    Raises:
+        ValueError: If the substitutions cannot be made due to missing variables.
+
+    Returns:
+        str: The template string with the appropriate substitutions made.
+    """
+    mapping = {**mapping, **kwds}
+
+    def _sub_curly(match: Match[str]) -> str:
+        # group(1) returns string without {}, group(0) returns with {}
+        # result is we only do replacements that we can actually do.
+        res = mapping.get(match.group(1))
+        if allow_missing and res is None:
+            res = match.group(0)
+        elif res is None:
+            raise ValueError
+        return res
+
+    def _sub_square(match: Match[str]) -> str:
+        # make curly substitutions inside of square brackets or remove the whole thing
+        # if substitutions cannot be made.
+        resolved = re.sub(_CURLY_BRACKET_REGEX, _sub_curly, match.group(1))
+        return resolved if resolved != match.group(1) else ""
+
+    squared = re.sub(_SQUARE_BRACKET_REGEX, _sub_square, template)
+    resolved = re.sub(_CURLY_BRACKET_REGEX, _sub_curly, squared)
+
+    return resolved
+
+
+class TemplateString:
+    """Python f-string implementation with lazy and optional variable substitutions.
+
+    The template string is expected to be formatted in the same way as python f-strings,
+    with variables that should be substituted wrapped in curly brances `{}`.
+    Additionally, square brackets may be used around curly brackets and other text to
+    mark that substitution as optional -- i.e. if the variable cannot be found then the
+    text wrapped in the square brackets will be removed.
+
+
+    Examples:
+
+        `mapping = dict(a="x", b="y", c="z")`
+
+        `TemplateString("{a}.{b}{c}w").substitute(mapping) # -> "x.yzw"`
+
+        `TemplateString("{a}.{b}[.{c}]").substitute(mapping) # -> "x.y.z"`
+
+        `TemplateString("{a}.{b}.{d}").substitute(mapping)  # raises ValueError`
+
+        `TemplateString("{a}.{b}[.{d}]").substitute(mapping) # -> "x.y"`
+
+        `TemplateString("{a}.{b}.{d}").substitute(mapping, True) # -> "x.y.{d}"`
+
+    Args:
+        template (str): The template string. Variables to substitute should be wrapped
+            by curly braces `{}`.
+    """
+
+    def __init__(self, template: str) -> None:
+        if not self._is_balanced(template):
+            raise ValueError(f"Unbalanced brackets in template string: '{template}'")
+        self.template = template
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.template!r})"
+
+    def __str__(self) -> str:
+        return self.template
+
+    @classmethod
+    def _is_balanced(cls, template: str):
+        queue: List[str] = []
+        for char in template:
+            if char in "{[":
+                queue.append("}" if char == "{" else "]")
+            elif char in "}]":
+                if not queue or char != queue.pop():
+                    return False
+        return len(queue) == 0
+
+    def substitute(
+        self, mapping: Dict[str, str], allow_missing: bool = False, **kwds: str
+    ) -> str:
+        """Substitutes variables in a template string.
+
+        Args:
+            mapping (Dict[str, str]): A key-value store of variable name to the value to
+                replace it with.
+            allow_missing (bool, optional): Allow variables outside of square brackets to be
+                missing, in which case they are left as-is, including the curly brackets.
+                This is intended to allow users to perform some variable substitutions
+                before all variables in the mapping are known. Defaults to False.
+            **kwds (Dict[str, str]): Optional extras to be merged into the variable mapping
+                dict. If a keyword passed here has the same name as a key in the mapping
+                dict, the value here would be used instead.
+
+        Raises:
+            ValueError: If the substitutions cannot be made due to missing variables.
+
+        Returns:
+            str: The template string with the appropriate substitutions made.
+        """
+        return _substitute(self.template, mapping, allow_missing, **kwds)
---
+++ src/test_template.py
@@ -0,0 +1,49 @@
+from typing import Dict
+import pytest
+
+from src.template_str import TemplateString
+
+
+@pytest.mark.parametrize(
+    ("expected", "template", "mapping"),
+    (
+        ("", "", dict()),
+        ("a", "a", dict()),
+        ("a", "{b}", dict(b="a")),
+        ("a", "[{b}]", dict(b="a")),
+        ("", "[{b}]", dict()),
+        ("ab", "{a}[{b}]", dict(a="a", b="b")),
+        ("a.b", "{a}[.{b}]", dict(a="a", b="b")),
+        ("defg", "{a}{b}{c}g", dict(a="d", b="e", c="f")),
+        ("d.e-gf", "{a}.{b}[-g{c}]", dict(a="d", b="e", c="f")),
+        ("d.e-gf", "{a}.{b}[-g{c}][-{d}]", dict(a="d", b="e", c="f")),
+    ),
+)
+def test_correctness(expected: str, template: str, mapping: Dict[str, str]):
+    assert TemplateString(template).substitute(mapping, True) == expected
+
+
+@pytest.mark.parametrize(
+    ("error", "template", "mapping", "allow_missing"),
+    (
+        (ValueError, "{a}", dict(), False),
+        (ValueError, "{a", dict(), False),
+        (ValueError, "[a}]", dict(), False),
+        (ValueError, "{a}{b}{c}", dict(), False),
+    ),
+)
+def test_failures(
+    error: Exception, template: str, mapping: Dict[str, str], allow_missing: bool
+):
+    with pytest.raises(error):
+        TemplateString(template).substitute(mapping, allow_missing=allow_missing)
+
+
+def test_repr():
+    template = TemplateString("{a}{b}{c}")
+    assert repr(template) == "TemplateString('{a}{b}{c}')"
+
+
+def test_str():
+    template = TemplateString("{a}{b}{c}")
+    assert str(template) == "{a}{b}{c}"
